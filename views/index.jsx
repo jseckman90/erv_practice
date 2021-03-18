1394c45945c55676c042c5da20e610d5f896540b
Josh Seckman <jseckman90@gmail.com>
2021-03-18 16:58:06 -0400

    Index template created

---
+++ views/index.jsx
@@ -0,0 +1,13 @@
+const React = require("react");
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from Index</h1>
+      </div>
+    );
+  }
+}
+
+module.exports = Index;
