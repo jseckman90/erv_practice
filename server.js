a19b81b77aefa9a94cd56d6ee3bc28d522ff468f
Josh Seckman <jseckman90@gmail.com>
2021-03-18 16:51:33 -0400

    simple server set up

---
+++ server.js
@@ -0,0 +1,21 @@
+// Express
+const express = require("express");
+const app = express();
+
+require("dotenv").config;
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.static("public")); // sets public folder to be served statically(img/css files)
+app.set("view engine", "jsx"); // tells Express that our view engine will use .jsx files
+app.engine("jsx", require("express-react-views").createEngine()); // sets view engine
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("testing connection");
+});
+
+// Server listener
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
