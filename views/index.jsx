ad18eac16b34bbc403344223de5ddfc2a20036ce
Josh Seckman <jseckman90@gmail.com>
2021-03-19 16:03:44 -0400

    rendered array of numbers in a list

--- views/index.jsx
+++ views/index.jsx
@@ -1,12 +1,19 @@
 const React = require("react");
 const Layout = require("./Layout.jsx");
 
+const myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 class Index extends React.Component {
   render() {
     return (
       <Layout>
         <div>
           <h1>{this.props.hello}</h1>
+          <ul>
+            {myArr.map((num) => (
+              <li>{num}</li>
+            ))}
+          </ul>
         </div>
       </Layout>
     );
